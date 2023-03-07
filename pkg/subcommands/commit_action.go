61db0b98faf3131ec5039376cc178582b510a7ec
Zheng Ziming <zzheng2@atlassian.com>
2023-03-08 00:21:46 +1100

    update

---
+++ pkg/subcommands/commit_action.go
@@ -0,0 +1,10 @@
+package subcommands
+
+import (
+	"github.com/jet/pkg/boundaries"
+	"github.com/urfave/cli/v2"
+)
+
+func Commit(ctx *cli.Context, fileSys boundaries.FileSystem) error {
+	return nil
+}
--- pkg/subcommands/init_action.go
+++ pkg/subcommands/init_action.go
@@ -1,10 +1,45 @@
 package subcommands
 
 import (
-    "errors"
-    "github.com/jet/pkg/boundaries"
+	"fmt"
+	"github.com/jet/pkg/boundaries"
+	"github.com/urfave/cli/v2"
 )
 
-func Init(fileSys boundaries.FileSystem) error {
-	return errors.New("error")
+/**
+Only cares about the happy path and omit the error handling for now.
+*/
+
+func Init(ctx *cli.Context, fileSys boundaries.FileSystem) error {
+	var dirs = []string{
+		"objects",
+		"refs",
+	}
+
+	//cwd, err := getCwd()
+	//if err != nil {
+	//    return fmt.Errorf("cannot get cwd: %w", err)
+	//}
+	//path := cwd + ctx.Args().Get(0) + ".jet"
+
+	for _, dir := range dirs {
+		err := fileSys.MakeDirs("./.jet/" + dir)
+		if err != nil {
+			// todo: understand what does %w do???
+			return fmt.Errorf("cannot create a new dir: %w", err)
+		}
+	}
+
+	fmt.Printf("Initialized empty jet repo")
+
+	return nil
 }
+
+//func getCwd() (string, error) {
+//	ex, err := os.Executable()
+//	if err != nil {
+//		return "", err
+//	}
+//	exPath := filepath.Dir(ex)
+//	return exPath, nil
+//}
